b6f7e31c973c72944182bb58f3f585fed0a7cfe9
ZIONDEVS <zionsoftwarehouse.dev@gmail.com>
2025-03-21 15:03:47 -0300

    Update  activities.tsx

--- src/app/components/ activities.tsx
+++ src/app/components/ activities.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules'; 
 
 const Activities = () => {
   // Array consistente de títulos
